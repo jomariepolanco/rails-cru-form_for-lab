185cba36bd7d0fbe554cbb7c76f2c61a6f056261
jomariepolanco <jomariepolanco@gmail.com>
2020-10-27 20:50:47 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists
+  post 'artists/new', to: 'artists#create'
+  post 'artists/:id/edit', to: 'artists#update'
+
+  resources :genres
+  post 'genres/new', to: 'genres#create'
+  post 'genres/:id/edit', to: 'genres#update'
+
+  resources :songs 
+  post 'songs/new', to: 'songs#create'
+  post 'songs/:id/edit', to: 'songs#update'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "Beyonce", bio: "singer")
+Artist.create(name: "Rihanna", bio: "singer")
+Artist.create(name: "Beirut", bio: "band")
+Artist.create(name: "Thrashers", bio: "band")
+Artist.create(name: "Nirvana", bio: "solo")
+
+Genre.create(name: "hip hop")
+Genre.create(name: "pop")
+Genre.create(name: "classical")
+Genre.create(name: "edm")
+Genre.create(name: "rock")
+
+Song.create(name: "I", artist_id: 1, genre_id: 1)
+Song.create(name: "D", artist_id: 2, genre_id: 2)
+Song.create(name: "G", artist_id: 3, genre_id: 3)
+Song.create(name: "A", artist_id: 4, genre_id: 4)
+Song.create(name: "F", artist_id: 5, genre_id: 5)
+Song.create(name: "K", artist_id: 1, genre_id: 3)
